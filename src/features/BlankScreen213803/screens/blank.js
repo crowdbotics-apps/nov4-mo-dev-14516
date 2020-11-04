2929940595b75ce9901f4483416ff90654fad2fb
Crowdbotics <team@crowdbotics.com>
2020-11-04 07:03:46 +0000

    Update components

--- src/features/BlankScreen213803/screens/blank.js
+++ src/features/BlankScreen213803/screens/blank.js
@@ -28,7 +28,9 @@ export default class Blank extends React.Component {
     DateTimePicker_9: new Date(""),
     TextInput_11: "",
     Switch_13: true,
-    TextInput_17: ""
+    CheckBox_32: true,
+    TextInput_17: "",
+    CheckBox_19: true
   }
 
   render = () => (
@@ -41,7 +43,6 @@ export default class Blank extends React.Component {
         onChangeText={nextValue => this.setState({ TextInput_7: nextValue })}
       />
       <DateTimePicker
-        showIcon={false}
         date={this.state.DateTimePicker_9}
         onDateChange={selectedDate =>
           this.setState({ DateTimePicker_9: selectedDate })
@@ -49,21 +50,31 @@ export default class Blank extends React.Component {
       />
       <TextInput
         placeholder="Number Input Placeholder"
-        keyboardType="numeric"
         value={this.state.TextInput_11}
         onChangeText={nextValue => this.setState({ TextInput_11: nextValue })}
       />
       <Switch
-        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
         value={this.state.Switch_13}
         onValueChange={nextChecked => this.setState({ Switch_13: nextChecked })}
       />
       <Slider value={50} minimumValue={0} maximumValue={100} />
+      <CheckBox
+        title="Checkbox"
+        checked={this.state.CheckBox_32}
+        onPress={nextChecked => this.setState({ CheckBox_32: nextChecked })}
+      />
       <TextInput
         placeholder="Sample text input placeholder"
-        multiline={true}
+        style={styles.TextInput_17}
         value={this.state.TextInput_17}
         onChangeText={nextValue => this.setState({ TextInput_17: nextValue })}
+      />
+      <CheckBox
+        title="Radio button"
+        checkedIcon="dot-circle-o"
+        uncheckedIcon="circle-o"
+        checked={this.state.CheckBox_19}
+        onPress={nextChecked => this.setState({ CheckBox_19: nextChecked })}
       />
     </View>
   )
@@ -74,6 +85,7 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
   Button_3: {},
   Text_5: {},
@@ -82,5 +94,16 @@ const styles = StyleSheet.create({
   TextInput_11: {},
   Switch_13: {},
   Slider_15: {},
-  TextInput_17: {}
+  TextInput_17: {},
+  View_1: {},
+  Button_3: {},
+  Text_5: {},
+  TextInput_7: {},
+  DateTimePicker_9: {},
+  TextInput_11: {},
+  Switch_13: {},
+  Slider_15: {},
+  CheckBox_32: {},
+  TextInput_17: { height: 100 },
+  CheckBox_19: {}
 })
